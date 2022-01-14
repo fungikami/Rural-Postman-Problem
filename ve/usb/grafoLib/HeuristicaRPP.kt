0bb9d10945f74ab6de918c46c9b1cd6e064a54e9
chrischriscris <18-10892@usb.ve>
2022-01-14 08:46:38 -0400

    example added, mapping done and tested

--- ve/usb/grafoLib/HeuristicaRPP.kt
+++ ve/usb/grafoLib/HeuristicaRPP.kt
@@ -17,7 +17,10 @@ import java.lang.StringBuilder
  */
 public class HeuristicaRPP {
     companion object {
-    
+        
+        private val mapa = HashMap<Int, Int>()
+        private var mapaInverso = IntArray(0)
+
         private fun ejecutarAlgoritmo(
             g: GrafoNoDirigido,
             R: MutableSet<Arista>,
@@ -66,6 +69,10 @@ public class HeuristicaRPP {
             return CicloEulerianoGrafoNoDirigido(gP).obtenerCicloEuleriano()
         }
 
+        private fun f(v: Int) = mapa[v]
+
+        private fun fInversa(v: Int) = mapaInverso[v]
+
         private fun esPar(g: GrafoNoDirigido): Boolean {
             val n = g.obtenerNumeroDeVertices()
             val paridad = BooleanArray(n) { true }
@@ -81,69 +88,111 @@ public class HeuristicaRPP {
             return paridad.all { it }
         }
 
-        @JvmStatic fun main(args: Array<String>) {
-            // Verificar argumentos
-            if (args.size != 2 || (args[0] != "a" && args[0] != "v")) {
-                println("Error: Usage >./runHeurRPP.sh [a|v] <ruta/a/instancia>")
-                exitProcess(1)
-            }
-
+        private fun extraerDatos(nombreArchivo: String): 
+            Pair<GrafoNoDirigido, MutableSet<Arista>> {
             // Verifica si el archivo existe
             if (!File(nombreArchivo).exists()) {
                 println("El archivo indicado en $nombreArchivo no existe o no se puede leer.")
                 exitProcess(1)
             }
 
-            val vertexScan = args[0] == "v"
-            val nombreArchivo = args[1]
-
+            // Salta Nombre y Comentario
             val sc = Scanner(FileInputStream(nombreArchivo))
-            sc.nextLine()   // Saltar Nombre
-            sc.nextLine()   // Saltar Comentario
+            sc.nextLine()   
+            sc.nextLine()
 
-            var aux = sc.nextLine()!!.split(" ") // Línea VERTICES : <nro. de vértices>
-            val numDeVertices = aux[aux.size - 1].strip()!!.toInt()
+            // Línea VERTICES : <nro. de vértices>
+            var aux = sc.nextLine()!!.split(" ") 
+            val numDeVertices = aux[aux.size - 1].trim().toInt()
             
             // Construye grafo G y conjunto R
             val g = GrafoNoDirigido(numDeVertices)
             val R = mutableSetOf<Arista>()
 
-            aux = sc.nextLine()!!.split(" ")    // Línea ARISTAS_REQ : <nro. de lados requeridos>
-            val aristasReq = aux[aux.size - 1].strip()!!.toInt() 
+            // Línea ARISTAS_REQ : <nro. de lados requeridos>
+            aux = sc.nextLine()!!.split(" ")
+            val aristasReq = aux[aux.size - 1].trim().toInt() 
 
-            aux = sc.nextLine()!!.split(" ")    // Línea ARISTAS_NOREQ : <nro. de lados no requeridos>
-            val aristasNoReq = aux[aux.size - 1].strip()!!.toInt() 
+            // Línea ARISTAS_NOREQ : <nro. de lados no requeridos>
+            aux = sc.nextLine()!!.split(" ")
+            val aristasNoReq = aux[aux.size - 1].trim().toInt() 
 
-            // Agregar aristas requeridas a G y R
-            sc.nextLine()   // Saltar LISTA_ARISTAS_REQ
+            val mapaLista = ArrayList<Int>()
+            var k = 0
+
+            // Salta LISTA_ARISTAS_REQ
+            sc.nextLine()
+
+            /* Agrega aristas requeridas a G y R y construye
+            un mapeo biyectivo de V a V_R */
             repeat(aristasReq) {
                 aux = Regex("[0-9]+").findAll(sc.nextLine()!!)
                     .map(MatchResult::value)
                     .toList()
                 
-                val a = Arista(
-                    aux[0].toInt(), 
-                    aux[1].toInt(), 
+                val (u, v, peso) = Triple(
+                    aux[0].toInt() - 1,
+                    aux[1].toInt() - 1, 
                     aux[2].toDouble()
                 )
+
+                val a = Arista(u, v, peso)
+
+                if (!mapa.containsKey(u)) {
+                    mapa[u] = k
+                    mapaLista.add(u)
+                    k++
+                }
+
+                if (!mapa.containsKey(v)) {
+                    mapa[v] = k
+                    mapaLista.add(v)
+                    k++
+                }
+
                 g.agregarArista(a)
                 R.add(a)
             }
 
-            // Agregar aristas no requeridas a G
-            sc.nextLine()   // Saltar LISTA_ARISTAS_NOREQ
-            repeat(aristasReq) {
+            mapaInverso = mapaLista.toIntArray()
+
+            // Salta LISTA_ARISTAS_NOREQ
+            sc.nextLine()
+
+            // Agrega aristas no requeridas a G
+            repeat(aristasNoReq) {
                 aux = Regex("[0-9]+").findAll(sc.nextLine()!!)
                     .map(MatchResult::value)
                     .toList()
                 
                 val a = Arista(
-                    aux[0].toInt(), 
-                    aux[1].toInt(), 
+                    aux[0].toInt() - 1, 
+                    aux[1].toInt() - 1, 
                     aux[2].toDouble()
                 )
                 g.agregarArista(a)
             }
+
+            return Pair(g, R)
+        }
+
+        @JvmStatic fun main(args: Array<String>) {
+            // Verifica argumentos
+            if (args.size != 2 || (args[0] != "a" && args[0] != "v")) {
+                println("Error: Usage >./runHeurRPP.sh [a|v] <ruta/a/instancia>")
+                exitProcess(1)
+            }
+
+            val vertexScan = args[0] == "v"
+            val (g, R) = extraerDatos(args[1])
+
+            println("Vertices del grafo: ${g.obtenerNumeroDeVertices()}")
+
+            val mapaCorrecto = (
+                (0 until mapaInverso.size).all { f(fInversa(it)) == it } &&
+                (mapaInverso.size == mapa.size)
+            )
+            println("Mapa construido correctamente: $mapaCorrecto")
 
             // Ejecuta el algoritmo RPP
             var ciclo: Iterable<Arista>
@@ -152,9 +201,9 @@ public class HeuristicaRPP {
             }
             
             // Imprime salida
-            ciclo.forEach { print("${it.cualquieraDeLosVertices()} ") }
+            ciclo.forEach { print("${it.cualquieraDeLosVertices() - 1} ") }
             val u = ciclo.last().cualquieraDeLosVertices()
-            println(ciclo.last().elOtroVertice(u))
+            println(ciclo.last().elOtroVertice(u) - 1)
             println(ciclo.sumOf { it.peso() }.toInt())
             println("%.3f segs.".format(ms / 1000.0))
         }
